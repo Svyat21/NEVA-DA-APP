70920e0ed43d6b8f04d894ac1f8b88ea96418a91
sursv <sursvyat@gmail.com>
2021-10-13 14:14:53 +0300

    added initializing view

--- nevada_app/nevada_shop/views.py
+++ nevada_app/nevada_shop/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import View
+import logging
 
-# Create your views here.
+logger = logging.getLogger(__name__)
+
+
+class HomeView(View):
+
+    def get(self, request):
+        context = {
+            'title_html': 'Главная',
+        }
+        return render(request, 'nevada_shop/index.html', context=context)
