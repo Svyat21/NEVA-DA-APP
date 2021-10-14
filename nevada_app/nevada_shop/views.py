df51ed9934afcb0515d95302e22a6c7dedbb5dbf
sursv <sursvyat@gmail.com>
2021-10-14 22:12:02 +0300

    preliminary version of the presentation for the main page

--- nevada_app/nevada_shop/views.py
+++ nevada_app/nevada_shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
+from .models import Group, Team, Genre, Repertoire, Price, CommonRider, Rider
 import logging
 
 logger = logging.getLogger(__name__)
@@ -8,7 +9,15 @@ logger = logging.getLogger(__name__)
 class HomeView(View):
 
     def get(self, request):
+        about = Group.objects.filter(pk=1)[0]
+        genre = Genre.objects.all().prefetch_related('repertoire_set')
+        price = Price.objects.filter(pk=1)[0]
+        rider = CommonRider.objects.filter(pk=1)[0]
         context = {
             'title_html': 'Главная',
+            'about': about,
+            'genre': genre,
+            'price': price,
+            'rider': rider,
         }
         return render(request, 'nevada_shop/index.html', context=context)
