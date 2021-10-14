93aa02d1b984dc041eaaf2febf2cea6eca882a88
sursv <sursvyat@gmail.com>
2021-10-14 15:43:17 +0300

    Setting up the admin panel

--- nevada_app/nevada_shop/admin.py
+++ nevada_app/nevada_shop/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Group, Team, Genre, Repertoire, Price, CommonRider, Rider
 
-# Register your models here.
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    list_display_links = ('id', 'name',)
+
+
+class RepertoireAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'genre',)
+    list_display_links = ('id', 'name',)
+
+
+class RiderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'main_rider',)
+    list_display_links = ('id', 'name',)
+
+
+admin.site.register(Group)
+admin.site.register(Team, TeamAdmin)
+admin.site.register(Genre)
+admin.site.register(Repertoire, RepertoireAdmin)
+admin.site.register(CommonRider)
+admin.site.register(Rider, RiderAdmin)
+admin.site.register(Price)
