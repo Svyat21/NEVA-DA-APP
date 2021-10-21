2f6616886cf477e8c337bb57f2c4a00be046da14
sursv <sursvyat@gmail.com>
2021-10-21 14:26:36 +0300

    added functionality for the expand button

--- nevada_app/nevada_shop/static/nevada_shop/js/showing_content.js
+++ nevada_app/nevada_shop/static/nevada_shop/js/showing_content.js
@@ -1,4 +1,4 @@
-let findButtoms = document.querySelectorAll('.find_buttom')
+let findButtoms = document.querySelectorAll('.find_buttom');
 
 if (findButtoms.length > 0) {
     for (let i = 0; i < findButtoms.length; i++) {
@@ -7,6 +7,25 @@ if (findButtoms.length > 0) {
             const repertoire = buttom.closest('.repertoire_element').querySelector('.repertoire_content');
             buttom.classList.toggle('find_after');
             repertoire.classList.toggle('invisible');
+        });
+    }
+}
+
+let findFullRiders = document.querySelectorAll('.rider_bottom');
+
+if (findFullRiders.length > 0) {
+    for (let i = 0; i < findFullRiders.length; i++) {
+        const buttom = findFullRiders[i];
+        buttom.addEventListener('click', function () {
+            const minRider = buttom.closest('.rider_full_body').querySelector('.text_rider_min');
+            const fullRider = buttom.closest('.rider_full_body').querySelector('.text_rider');
+            minRider.classList.toggle('invisible');
+            fullRider.classList.toggle('invisible');
+            if (buttom.innerHTML === 'Развернуть') {
+                buttom.innerHTML = 'Свернуть';
+            } else {
+                buttom.innerHTML = 'Развернуть';
+            }
         });
     }
 }
