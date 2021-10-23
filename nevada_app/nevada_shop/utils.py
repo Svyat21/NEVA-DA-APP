4f3e20ac40b2c5aa9c339b0ec1a485c5b6b9f0bd
sursv <sursvyat@gmail.com>
2021-10-23 18:03:07 +0300

    added the function of sending a feedback message

---
+++ nevada_app/nevada_shop/utils.py
@@ -0,0 +1,15 @@
+from django.core.mail import send_mail
+
+
+def feedback_message(dict_message: dict):
+    message = f"Заказан звонок от пользователя:\n{dict_message['name']}\n" \
+              f"Номер телефона:\n{dict_message['phone']}\n"
+    if dict_message.get('question'):
+        message += f"Вопрос от пользователя:\n{dict_message['question']}"
+    send_mail(
+        subject=f"Заказ обратной связи от пользователя {dict_message['name']}",
+        message=message,
+        from_email='no-reply@nevada-band.ru',
+        recipient_list=['sursvyat@gmail.com'],
+        fail_silently=False,
+    )
