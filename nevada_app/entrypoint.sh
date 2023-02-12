87aefef7ebe24771849932862673fba5c58ae5d6
Svyatoslav Surovenkov <sursvyat@gmail.com>
2023-02-12 11:24:59 +0300

    закомментированы миграции

--- nevada_app/entrypoint.sh
+++ nevada_app/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-python manage.py makemigrations --no-input
-python manage.py migrate --no-input
+#python manage.py makemigrations --no-input
+#python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 # python manage.py createsuperuser --noinput --username svyat --email sursvyat@gmail.com
 
